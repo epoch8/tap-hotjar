05b36948f7fca2599533ba00e412ef660806a953
Ilya Sapunov <ilya.v.sapunov@gmail.com>
2023-05-26 16:13:59 +0300

    add b2c prod es

--- tap_hotjar/streams.py
+++ tap_hotjar/streams.py
@@ -339,6 +339,25 @@ class B2CProdEN(SurveysStream):
         th.Property(clean("Help us make your experience better! Are you professional or consumer?"), th.StringType),
     ).to_dict()
 
+class B2CProdES(SurveysStream):
+    name = "survey_b2c_prod_es"
+    site_id = "3356229"
+    survey_id = "904009"
+    schema = th.PropertiesList(
+        th.Property("Number", th.IntegerType),
+        th.Property("User", th.StringType),
+        th.Property("Date Submitted", th.StringType),
+        th.Property("Country", th.StringType),
+        th.Property("Source URL", th.StringType),
+        th.Property("Device", th.StringType),
+        th.Property("Browser", th.StringType),
+        th.Property("OS", th.StringType),
+        th.Property("Hotjar User ID", th.StringType),
+        th.Property(clean("¿Qué probabilidad hay que nos recomiendes a un amigo?"), th.StringType),
+        th.Property(clean("¿Cuál es la razón de tu respuesta?"), th.StringType),
+        th.Property(clean("¡Ayúdanos a mejorar tu experiencia! ¿Eres profesional o particular?"), th.StringType),
+    ).to_dict()
+
 
 class B2CRentFR(SurveysStream):
     name = "survey_b2c_rent_fr"
