c06b39f29188fd46dd4c7afecebd684721ea888f
Alexandr Filatov <alxndr.fltv@gmail..com>
2024-05-07 12:42:24 +0300

    Added configurations for romanian stream in tap_hotjar

--- tap_hotjar/streams.py
+++ tap_hotjar/streams.py
@@ -993,3 +993,23 @@ class B2C_PROD_ES_SV(SurveysStream):
         th.Property(clean("¿Cuál es la razón de tu respuesta?"), th.StringType),
         th.Property(clean("¡Ayúdanos a mejorar tu experiencia! ¿Eres profesional o particular?"), th.StringType),
     ).to_dict()
+
+
+class B2C_PROD_ROMANIAN(SurveysStream):
+    name = "survey_b2c_prod_romanian"
+    site_id = "3356229"
+    survey_id = "992131"
+    schema = th.PropertiesList(
+        th.Property("Number", th.IntegerType),
+        th.Property("User", th.StringType),
+        th.Property("Date Submitted", th.StringType),
+        th.Property("Country", th.StringType),
+        th.Property("Source URL", th.StringType),
+        th.Property("Device", th.StringType),
+        th.Property("Browser", th.StringType),
+        th.Property("OS", th.StringType),
+        th.Property("Hotjar User ID", th.StringType),
+        th.Property(clean("Cât de probabil este să ne recomanzi unui prieten sau coleg?"), th.NumberType),
+        th.Property(clean("Care este motivul evaluării tale?"), th.StringType),
+        th.Property(clean("Ajută-ne să ne îmbunătățim experiența! Ești profesionist sau consumator?"), th.StringType),
+    ).to_dict()
